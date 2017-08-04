28b28c63dd06c183dfc876444c8e977ca1f5e6e6
RandomRedstone <RandomRedstoneStudios@gmail.com>
2017-08-04 17:15:59 -0400

    Base
    Beginning of program

---
+++ 2018/src/Robot.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <IterativeRobot.h>
+#include <LiveWindow/LiveWindow.h>
+#include <SmartDashboard/SendableChooser.h>
+#include <SmartDashboard/SmartDashboard.h>
+
+class Robot: public frc::IterativeRobot {
+public:
+	void RobotInit() {
+
+	}
+	void AutonomousInit() override {
+
+	}
+	void AutonomousPeriodic() {
+
+	}
+	void TeleopInit() {
+
+	}
+	void TeleopPeriodic() {
+
+	}
+	void TestPeriodic() {
+
+	}
+};
